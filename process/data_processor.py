c57420e9b43877bac6237bad2b0a17bfdd859bd7
Nathan Smith <nwraysmith@gmail.com>
2021-04-14 16:12:19 -0700

    add subblock capability

--- process/data_processor.py
+++ process/data_processor.py
@@ -1,5 +1,5 @@
 """
-loads and preprocesses the structured log data for anomoly prediction
+loads and preprocesses the structured log data for anomaly prediction
 """
 import numpy as np
 import pandas as pd
@@ -20,7 +20,7 @@ def collect_event_ids(data_frame, regex_pattern, column_names):
         blk_id_list = re.findall(regex_pattern, row["Content"])
         blk_id_set = set(blk_id_list)
         for blk_id in blk_id_set:
-            if not blk_id in data_dict:
+            if blk_id not in data_dict:
                 data_dict[blk_id] = []
             data_dict[blk_id].append(row["EventId"])
     data_df = pd.DataFrame(list(data_dict.items()), columns=column_names)
@@ -70,6 +70,60 @@ class FeatureExtractor(object):
         print("Train data shape: {}-by-{}\n".format(X_new.shape[0], X_new.shape[1]))
         return X_new
 
+    def fit_transform_subblocks(self, X_seq, term_weighting=None):
+        """
+        Fit and transform the training set
+        X_Seq: ndarray,  log sequences matrix
+        term_weighting: None or `tf-idf`
+        normalization: None - not implemented yet
+        """
+        self.term_weighting = term_weighting
+
+        # get unique events
+        unique_events = set()
+        for i in X_seq:
+            unique_events.update(i)
+        self.events = unique_events
+
+        # Convert into bag of words
+        all_blocks_count = []
+        for block in X_seq:
+            # multiply block by 20 for 5% partitions
+            block_rep = np.repeat(block, 20)
+            # now split into 5% partitions
+            block_split = np.split(block_rep, 20)
+            block_counts = []
+            for sub_block in block_split:
+                # count each sub_block
+                subset_count = Counter(sub_block)
+                block_counts.append(subset_count)
+            # put into dataframe to add nas to missing events
+            # divide by 20 as original operation multiplied by 20
+            block_df = pd.DataFrame(block_counts, columns=self.events) / 20
+            block_np = block_df.to_numpy()
+            all_blocks_count.append(block_np)
+
+        # finally stack the blocks
+        X = np.stack(all_blocks_count)
+
+        # CODE BELOW HERE IS NOT UPDATED
+        # NEED TO 1: GET DOCUMENT COUNT (COULD POTENTIALLY BE INCORPORATED IN THE ABOVE BLOCK)
+        # 2: APPLY THE SLIDE WINDOW SUCH THAT THE FINAL RESULT IS 20 ROWS OF 0-10, 5-15, 10-25
+        # INSTEAD OF THE CURRENT 0-5, 5-10, 10-15 (CAN'T DO THIS BEFORE DOCUMENT COUNT)
+        # 3: APPLY DOCUMENT VECTOR
+        # STEPS 1 AND 3 SHOULD BE EASYISH
+        num_instance, num_event = X.shape
+        # applies tf-idf if pararmeter
+        if self.term_weighting == "tf-idf":
+            df_vec = np.sum(X > 0, axis=0)
+            self.idf_vec = np.log(num_instance / (df_vec + 1e-8))
+            idf_matrix = X * np.tile(self.idf_vec, (num_instance, 1))
+            X = idf_matrix
+
+        X_new = X
+        print("Train data shape: {}-by-{}\n".format(X_new.shape[0], X_new.shape[1]))
+        return X_new
+
     def transform(self, X_seq):
         """
         transforms x test
@@ -88,7 +142,45 @@ class FeatureExtractor(object):
             X_df[event] = [0] * len(X_df)
         X = X_df[self.events].values
 
-        # applies tf-idf if pararmeter
+        # applies tf-idf if parameter
+        num_instance, _ = X.shape
+        if self.term_weighting == "tf-idf":
+            idf_matrix = X * np.tile(self.idf_vec, (num_instance, 1))
+            X = idf_matrix
+
+        X_new = X
+        print("Test data shape: {}-by-{}\n".format(X_new.shape[0], X_new.shape[1]))
+        return X_new
+
+    def transform_subblock(self, X_seq):
+        """
+        transforms x test
+        X_seq: log sequence data
+        """
+
+        # Convert into bag of words
+        all_blocks_count = []
+        for block in X_seq:
+            # multiply block by 20 for 5% partitions
+            block_rep = np.repeat(block, 20)
+            # now split into 5% partitions
+            block_split = np.split(block_rep, 20)
+            block_counts = []
+            for sub_block in block_split:
+                # count each sub_block
+                subset_count = Counter(sub_block)
+                block_counts.append(subset_count)
+            # put into dataframe to add nas to missing events
+            # divide by 20 as original operation multiplied by 20
+            block_df = pd.DataFrame(block_counts, columns=self.events) / 20
+            block_np = block_df.to_numpy()
+            all_blocks_count.append(block_np)
+
+        # finally stack the blocks
+        X = np.stack(all_blocks_count)
+
+        # CODE BELOW HERE NOT MODIFIED
+        # applies tf-idf if parameter
         num_instance, _ = X.shape
         if self.term_weighting == "tf-idf":
             idf_matrix = X * np.tile(self.idf_vec, (num_instance, 1))
