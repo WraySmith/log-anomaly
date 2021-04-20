2bd325aa8e489f58cb6a02ea41b7b8053974f45a
Mitch Harris <mitchharris@live.ca>
2021-04-19 19:19:20 -0700

    remove val set from train set

--- process/val_set_creator.py
+++ process/val_set_creator.py
@@ -18,6 +18,8 @@ if __name__ == "__main__":
     x_train = np.load("{}x_train_tf-idf_rolling_v2.npy".format(data_location))
     y_train = pd.read_csv("{}y_train_tf-idf_rolling_v2.csv".format(data_location))
 
+    old_x_train_shape = x_train.shape
+    old_y_train_shape = y_train.shape
     print("x_train shape")
     print(x_train.shape)
     print("y_train shape")
@@ -45,6 +47,22 @@ if __name__ == "__main__":
 
     assert y_val.iloc[0].equals(y_train.iloc[val_idx[0]])
     assert np.array_equal(x_val[0], x_train[val_idx[0]])
+
+    anomalies = y_val[y_val["Label"] == "Anomaly"]
+    print("number of anomalies in val set", anomalies.shape)
+    print(
+        "percentage of records that are anonylous in val set",
+        anomalies.shape[0] / y_val.shape[0],
+    )
+
+    # delete from x_train, y_train the val_idx
+    x_train = np.delete(x_train, val_idx, axis=0)
+    y_train = y_train.drop(y_train.index[[val_idx]])
+
+    print(x_train.shape[0], x_val.shape[0], old_x_train_shape[0])
+    print(y_train.shape[0], y_val.shape[0], old_y_train_shape[0])
+    assert x_train.shape[0] + x_val.shape[0] == old_x_train_shape[0]
+    assert y_train.shape[0] + y_val.shape[0] == old_y_train_shape[0]
 
     print("loaded, split, and checked")
     np.save("{}x_val_tf-idf_rolling_v2.npy".format(data_location), x_val)
